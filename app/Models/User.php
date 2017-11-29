092c19844228ab14657d35c9f22a57c0eccf1466
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2017-11-29 12:51:02 +0100

    Creado modelo, migraciones y seeder de tipos de suscripcion

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'email', 'password', 'email_token',
+        'name', 'surname', 'email', 'password', 'email_token', 'suscripcion_id',
     ];
 
     /**
