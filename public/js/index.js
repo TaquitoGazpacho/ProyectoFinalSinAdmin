313afed4febfa49daf9a89d953bc7910cdf39424
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2017-11-29 13:29:03 +0100

    home view

--- public/js/index.js
+++ public/js/index.js
@@ -76,3 +76,8 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+$('#userTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,123 @@
 @extends('fijas.master')
 
-@section('principal')
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+@section('section')
 
-                    <div class="panel-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                            @if(Auth::guard('admin')->check())
-                                Hola {{ Auth::guard('admin')->user()->name}}
-                            @elseif(Auth::guard('web')->check())
-                                <p>Hola {{ Auth::guard('web')->user()->name}}</p>
-                                Eres usuario
-                                @if ( Auth::guard('web')->user()->invitado)
-                                    INVITADO
-                                @else
-                                    NORMAL
-                                @endif
-                            @endif
+    <div class="container well">
+        <ul class="nav nav-tabs" role="tablist" id="userTabs">
+            <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+            <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+            <li role="presentation"><a href="#suscripcion" aria-controls="suscripcion" role="tab" data-toggle="tab">Suscripcion</a></li>
+            <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            {{--HOME--}}
+            <div role="tabpanel" class="tab-pane active" id="home">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
                     </div>
-                </div>
+                @endif
+                @if(Auth::guard('admin')->check())
+                    Hola {{ Auth::guard('admin')->user()->name}}
+                @elseif(Auth::guard('web')->check())
+                    <p>Hola {{ Auth::guard('web')->user()->name}}</p>
+                    Suscripcion {{Auth::guard('web')->user()->suscripcion->name}}<br/>
+                    Eres usuario
+                    @if ( Auth::guard('web')->user()->invitado)
+                        INVITADO
+                    @else
+                        NORMAL
+                    @endif
+                @endif
             </div>
+            <div role="tabpanel" class="tab-pane" id="profile">
+                Dola
+            </div>
+            {{--SUSCRIPCIONES--}}
+            <div role="tabpanel" class="tab-pane" id="suscripcion">
+                <div class="container">
+                    <div class="heading">
+                        <h2 style="color: #F96F00;"><strong>Planes a gusto de todos</strong></h2>
+                        <p>Nos adaptamos a tus necesidades</p>
+                    </div><!-- //end heading -->
+                    <div class="row db-padding-btm db-attached">
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <div class="db-wrapper">
+                                <div class="db-pricing-eleven db-bk-color-one">
+                                    <div class="price">
+                                        10<sup>€</sup>
+                                        <small>anuales</small>
+                                    </div>
+                                    <div class="type">
+                                        Básico
+                                    </div>
+                                    <ul>
+
+                                        <li><i class="glyphicon glyphicon-print"></i>1 taquilla</li>
+                                        <li><i class="glyphicon glyphicon-time"></i>Usos ilimitados</li>
+                                        <li><i class="glyphicon glyphicon-trash"></i>Tamaño taquillas:<br>Pequeña y mediana</li>
+                                    </ul>
+                                    <div class="pricing-footer">
+
+                                        <a href="{{route('register')}}" class="btn db-button-color-square btn-lg">Contrata Plan</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <div class="db-wrapper">
+                                <div class="db-pricing-eleven db-bk-color-two popular">
+                                    <div class="price">
+                                        20<sup>€</sup>
+                                        <small>anuales</small>
+                                    </div>
+                                    <div class="type">
+                                        Premium
+                                    </div>
+                                    <ul>
+
+                                        <li><i class="glyphicon glyphicon-print"></i>3 taquilla</li>
+                                        <li><i class="glyphicon glyphicon-time"></i>Usos ilimitados</li>
+                                        <li><i class="glyphicon glyphicon-trash"></i>Tamaño taquillas:<br>Peuqeña, mediana y grande</li>
+                                    </ul>
+                                    <div class="pricing-footer">
+
+                                        <a href="{{route('register')}}" class="btn db-button-color-square btn-lg">Contrata Plan</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <div class="db-wrapper">
+                                <div class="db-pricing-eleven db-bk-color-three">
+                                    <div class="price">
+                                        150<sup>€</sup>
+                                        <small>anuales</small>
+                                    </div>
+                                    <div class="type">
+                                        Empresas
+                                    </div>
+                                    <ul>
+
+                                        <li><i class="glyphicon glyphicon-print"></i>10 taquillas</li>
+                                        <li><i class="glyphicon glyphicon-time"></i>Usos ilimitados</li>
+                                        <li><i class="glyphicon glyphicon-trash"></i>Tamaños extra aparte de los regulares, bajo necesidad.</li>
+                                    </ul>
+                                    <div class="pricing-footer">
+
+                                        <a href="{{route('register')}}" class="btn db-button-color-square btn-lg">Contrata Plan</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- FIN PRECIO PLANES -->
+            </div>
+            {{--SETTINGS--}}
+            <div role="tabpanel" class="tab-pane" id="settings">...</div>
         </div>
     </div>
-</header>
+
 @endsection
