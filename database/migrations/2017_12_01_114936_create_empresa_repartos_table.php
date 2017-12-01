46592a3df87ee1b178bb9382ade8c0324d8cd695
iker <ikerruzo91@gmail.com>
2017-12-01 13:09:03 +0100

    Modelo y migración empresa_reparto

---
+++ database/migrations/2017_12_01_114936_create_empresa_repartos_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresaRepartosTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('empresa_repartos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('email')->unique();
+            $table->string('telefono')->unique();
+            $table->string('nif')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('empresa_repartos');
+    }
+}
