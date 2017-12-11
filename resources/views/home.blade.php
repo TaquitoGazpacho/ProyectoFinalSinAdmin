42888aa58476c3023c6e2cd6d6580e5a19563e4c
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2017-12-11 08:14:18 +0100

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
             <div role="tabpanel" class="tab-pane" id="profile">
                 <div class="row">
                     <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
-                        <img src="https://www.svgimages.com/svg-image/s5/man-passportsize-silhouette-icon-256x256.png" alt="stack photo" class="img">
+                        <img src="{{Auth::guard('web')->user()->image}}" alt="stack photo" class="img">
                     </div>
                     <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
                         <div style="border-bottom:1px solid black">
@@ -51,14 +51,20 @@
                         </div>
                         <br/>
                         <ul class="details">
-                            <li><p><span class="glyphicon glyphicon-earphone one" style="width:50px;"></span>+91 90000 00000</p></li>
+                            @if(Auth::guard('web')->check())
+                                <li><p><span class="glyphicon glyphicon-earphone one" style="width:50px;"></span>{{Auth::guard('web')->user()->phone}}</p></li>
+                            @endif
                             @if(Auth::guard('admin')->check())
                                 <li><p><span class="glyphicon glyphicon-envelope one" style="width:50px;"></span>{{Auth::guard('admin')->user()->email}}</p></li>
                             @elseif(Auth::guard('web')->check())
                                 <li><p><span class="glyphicon glyphicon-envelope one" style="width:50px;"></span>{{Auth::guard('web')->user()->email}}</p></li>
                             @endif
-                            <li><p><span class="glyphicon glyphicon-map-marker one" style="width:50px;"></span>Hyderabad</p></li>
-                            <li><p><span class="glyphicon glyphicon-new-window one" style="width:50px;"></span><a href="#">www.example.com</a></p></li>
+                            @if(Auth::guard('web')->check())
+                                <li><p><span class="glyphicon glyphicon-earphone one" style="width:50px;"></span>{{Auth::guard('web')->user()->sex}}</p></li>
+                            @endif
+                                @if(Auth::guard('web')->check())
+                                    <li><p><span class="glyphicon glyphicon-earphone one" style="width:50px;"></span>Suscripción: {{Auth::guard('web')->user()->suscripcion->name}}</p></li>
+                                @endif
                         </ul>
                         <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">Edit Profile</button>
 
