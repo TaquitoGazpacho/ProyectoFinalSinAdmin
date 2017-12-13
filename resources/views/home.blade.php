34a5df5dafdfdb6e9143c98c72ba69a65b2f7951
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2017-12-13 10:29:43 +0100

    vista home de usuario con imagen responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
             <div role="tabpanel" class="tab-pane" id="profile">
                 <div class="row">
                     <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
-                        <img src="{{Auth::guard('web')->user()->image}}" alt="stack photo" class="img">
+                        <img src="{{Auth::guard('web')->user()->image}}" alt="stack photo" class="img img-responsive" >
                     </div>
                     <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
                         <div style="border-bottom:1px solid black">
